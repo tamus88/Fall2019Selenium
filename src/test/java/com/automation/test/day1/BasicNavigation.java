9147733f61191658c115515a9367a8f8405e7881
tamus88 <tamus88@mail.ru>
2020-03-01 15:47:16 -0500

    updated

--- src/test/java/com/automation/test/day1/BasicNavigation.java
+++ src/test/java/com/automation/test/day1/BasicNavigation.java
@@ -11,6 +11,7 @@ public class BasicNavigation {
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
 
+        driver.manage().window().maximize();
         Thread.sleep(3000);
         String title = driver.getTitle();
         String expectedTitle = "Google";
@@ -20,7 +21,26 @@ public class BasicNavigation {
         }else{
             System.out.println("Test failed");
         }
+        driver.navigate().to("http://amazon.com");
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test failed");
+        }
+        driver.navigate().back();
+       driver.navigate().refresh();
+        Thread.sleep(3000);
+        verifyEquals(driver.getTitle(),"Google");
+        System.out.println("URL : " + driver.getCurrentUrl());
+        driver.navigate().forward();
+
         driver.close();
     }
-
+        public static void verifyEquals(String arg1,String arg2){
+        if(arg1.equals(arg2)){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test failed");
+        }
+        }
 }
